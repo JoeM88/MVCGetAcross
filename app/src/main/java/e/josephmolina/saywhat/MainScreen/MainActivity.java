790660d5943c6f957d8fb125dd93a77770d6a0f0
Joseph Molina <josephmolina3388@gmail.com>
2018-06-12 18:45:34 -0700

    Added menu inflator

--- app/src/main/java/e/josephmolina/saywhat/MainScreen/MainActivity.java
+++ app/src/main/java/e/josephmolina/saywhat/MainScreen/MainActivity.java
@@ -2,9 +2,12 @@ package e.josephmolina.saywhat.MainScreen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.crashlytics.android.Crashlytics;
 
+import e.josephmolina.saywhat.R;
 import io.fabric.sdk.android.Fabric;
 
 
@@ -17,5 +20,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         controller = new MainController(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_layout, menu);
+        return true;
     }
 }
