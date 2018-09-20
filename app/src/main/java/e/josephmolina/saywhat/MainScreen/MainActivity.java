56cb1816d52e27e1089771d8e90d6934e57e9c8b
Joseph Molina <josephmolina3388@gmail.com>
2018-09-20 14:57:55 -0700

    Cleaned mainactivity

--- app/src/main/java/e/josephmolina/saywhat/MainScreen/MainActivity.java
+++ app/src/main/java/e/josephmolina/saywhat/MainScreen/MainActivity.java
@@ -1,10 +1,10 @@
 package e.josephmolina.saywhat.MainScreen;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -26,7 +26,6 @@ public class MainActivity extends AppCompatActivity implements SayWhatDialog.Dia
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         controller = new MainController(this);
-
     }
 
     @Override
