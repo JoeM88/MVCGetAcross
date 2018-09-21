783c6f7427b8260615d8e1e1e74713e10b220383
Joseph Molina <josephmolina3388@gmail.com>
2018-09-20 19:42:24 -0700

    Added methods to create intents into utils

--- app/src/main/java/e/josephmolina/saywhat/Utils/Utils.java
+++ app/src/main/java/e/josephmolina/saywhat/Utils/Utils.java
@@ -1,9 +1,16 @@
 package e.josephmolina.saywhat.Utils;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
+import android.speech.RecognizerIntent;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
+
+import e.josephmolina.saywhat.MainScreen.MainActivity;
+import e.josephmolina.saywhat.SavedChatsScreen.SavedTranslationsScreen;
 
 public final class Utils {
 
@@ -32,5 +39,27 @@ public final class Utils {
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         return activeNetwork != null &&
                 activeNetwork.isConnectedOrConnecting();
+    }
+
+    public static void onScreenPressed(MainActivity mainActivity) {
+        InputMethodManager imm = (InputMethodManager) mainActivity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(mainActivity.getCurrentFocus().getWindowToken(), 0);
+    }
+
+    public static void launchSavedTranslationsScreen(MainActivity mainActivity) {
+        Intent goToSavedTranslationScreen = new Intent(mainActivity, SavedTranslationsScreen.class);
+        mainActivity.startActivity(goToSavedTranslationScreen);
+    }
+
+    public static Intent createVoiceInputIntent() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        return intent;
+    }
+
+    public static Intent createYandexCreditIntent() {
+        Uri webpage = Uri.parse("http://translate.yandex.com/");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        return intent;
     }
 }
