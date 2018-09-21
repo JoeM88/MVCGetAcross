1b89bd88e5da3d131ec85728dd7f04834ca4a3a4
Joseph Molina <josephmolina3388@gmail.com>
2018-09-20 19:42:24 -0700

    Added methods to create intents into utils

---
+++ app/src/main/java/e/josephmolina/saywhat/AWSModel/AmazonTranslateClient.java
@@ -0,0 +1,40 @@
+package e.josephmolina.saywhat.Model;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.services.translate.AmazonTranslateAsyncClient;
+
+public class AmazonTranslateClient {
+    private static AWSCredentials awsCredentials;
+    private static AmazonTranslateAsyncClient amazonTranslateAsyncClient;
+
+    private AmazonTranslateClient() {
+
+    }
+
+    private static AWSCredentials getAWSCredsInstance() {
+        if (awsCredentials != null) {
+            return awsCredentials;
+        }
+        awsCredentials = new AWSCredentials() {
+            @Override
+            public String getAWSAccessKeyId() {
+                return "AKIAJGCDA5QOOXDIBGGQ";
+            }
+
+            @Override
+            public String getAWSSecretKey() {
+                return  "/sLymCNHHT0n/qGMCqkD7j+hzZ/r/wIoJQF3DjAm";
+            }
+        };
+        return awsCredentials;
+
+    }
+
+    public static AmazonTranslateAsyncClient getAmazonTranslateAsyncClientInstance() {
+        if (amazonTranslateAsyncClient != null) {
+            return amazonTranslateAsyncClient;
+        }
+        amazonTranslateAsyncClient = new AmazonTranslateAsyncClient(getAWSCredsInstance());
+        return amazonTranslateAsyncClient;
+    }
+}
