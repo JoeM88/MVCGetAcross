f83131a969f67b9d3e986fbbe4fd99c15822ba58
Joseph Molina <josephmolina3388@gmail.com>
2019-04-03 22:32:29 -0700

    Fixing git PR comments

--- app/src/main/java/e/josephmolina/saywhat/Modules/SayWhatApp.java
+++ app/src/main/java/e/josephmolina/saywhat/Modules/SayWhatApp.java
@@ -10,10 +10,8 @@ public class SayWhatApp extends Application {
         super.onCreate();
         if (LeakCanary.isInAnalyzerProcess(this)) {
             // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
             return;
         }
         LeakCanary.install(this);
-        // Normal app init code...
     }
 }
