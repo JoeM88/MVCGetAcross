c23d7ca5c9ecb6aa84b832cfb8402b37546b8751
aymswick <aymswick@aymswicks-MacBook-Pro.local>
2019-04-04 15:49:23 -0700

    Fixed the invisible speak button on main screen layout

--- app/src/main/java/e/josephmolina/saywhat/Dialog/SayWhatDialog.java
+++ app/src/main/java/e/josephmolina/saywhat/Dialog/SayWhatDialog.java
@@ -1,11 +1,11 @@
 package e.josephmolina.saywhat.Dialog;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 
 import e.josephmolina.saywhat.R;
